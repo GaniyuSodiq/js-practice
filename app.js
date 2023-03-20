bed194d52d625d796a93017d404cc5a37dd772c5
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-20 07:19:02 +0100

    tried in html function call

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let purchase = document.getElementById("purchase")
 let error = document.getElementById("error")
 
-purchase.addEventListener("click", function(){
+// purchase.addEventListener("click", function(){
+//     error.textContent = "Something went wrong, please try again"
+// })
+
+function purchaseF(){
     error.textContent = "Something went wrong, please try again"
-})
+}
