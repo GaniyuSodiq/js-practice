0560b33e6419dd5204e4f87f540740d249817465
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-20 09:46:18 +0100

    if else practice

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-let sumEl = document.getElementById("sum-el")
+// let num1 = 8
+// let num2 = 2
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
+// let sumEl = document.getElementById("sum-el")
 
-function add(){
-    sumEl.textContent = "Sum: "
-    let addition = num1 + num2
-    sumEl.textContent += addition
-}
+// function add(){
+//     sumEl.textContent = "Sum: "
+//     let addition = num1 + num2
+//     sumEl.textContent += addition
+// }
 
-function substract(){
-    sumEl.textContent = "Sum: "
-    let substraction = num1 - num2
-    sumEl.textContent += substraction
-}
+// function substract(){
+//     sumEl.textContent = "Sum: "
+//     let substraction = num1 - num2
+//     sumEl.textContent += substraction
+// }
 
-function divide(){
-    sumEl.textContent = "Sum: "
-    let divition = num1 / num2
-    sumEl.textContent += divition
-}
+// function divide(){
+//     sumEl.textContent = "Sum: "
+//     let divition = num1 / num2
+//     sumEl.textContent += divition
+// }
 
-function multiply(){
-    sumEl.textContent = "Sum: "
-    let multiplication = num1 * num2
-    sumEl.textContent += multiplication
+// function multiply(){
+//     sumEl.textContent = "Sum: "
+//     let multiplication = num1 * num2
+//     sumEl.textContent += multiplication
+// }
+
+let age = 11
+
+if (age < 21) {
+    console.log("You cannot enter the club!")
+} else {
+    console.log("Welcome!")
 }
