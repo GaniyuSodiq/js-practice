a7f014add4cf388341bf3fc86ce890d32a0d1c55
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-20 07:48:42 +0100

    created the muni calculator app

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-let purchase = document.getElementById("purchase")
-let error = document.getElementById("error")
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
 
-// purchase.addEventListener("click", function(){
-//     error.textContent = "Something went wrong, please try again"
-// })
+function add(){
+    sumEl.textContent = "Sum: "
+    let addition = num1 + num2
+    sumEl.textContent += addition
+}
 
-function purchaseF(){
-    error.textContent = "Something went wrong, please try again"
+function substract(){
+    sumEl.textContent = "Sum: "
+    let substraction = num1 - num2
+    sumEl.textContent += substraction
+}
+
+function divide(){
+    sumEl.textContent = "Sum: "
+    let divition = num1 / num2
+    sumEl.textContent += divition
+}
+
+function multiply(){
+    sumEl.textContent = "Sum: "
+    let multiplication = num1 * num2
+    sumEl.textContent += multiplication
 }
