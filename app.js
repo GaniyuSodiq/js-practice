e009503461170354e2363fdc65e406fb6ef9cf05
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-24 06:52:32 +0100

    dom + array + for loop exercise

--- app.js
+++ app.js
@@ -107,20 +107,28 @@
 //     console.log(i)
 // }
 
-let messages = [
-    "hey, how is it going?",
-    "i'm great, how about you!",
-    "All good. Been working on my portfolio lately"
-]
+// let messages = [
+//     "hey, how is it going?",
+//     "i'm great, how about you!",
+//     "All good. Been working on my portfolio lately"
+// ]
 
-messages.push("Same Here")
+// messages.push("Same Here")
 
-for (let i = 0; i < messages.length; i++){
-    console.log(messages[i])
-}
+// for (let i = 0; i < messages.length; i++){
+//     console.log(messages[i])
+// }
 
-let cards = [7, 3, 9]
+// let cards = [7, 3, 9]
 
-for (let i = 0; i < cards.length; i ++) {
-    console.log("number " + cards[i])
+// for (let i = 0; i < cards.length; i ++) {
+//     console.log("number " + cards[i])
+// }
+
+let sentence = ["Hello", "my", "name", "is", "Sodiq"]
+
+let greetingEl = document.getElementById("greeting-el")
+
+for (let i = 0; i < sentence.length; i++){
+    greetingEl.textContent += sentence[i] + " "
 }
